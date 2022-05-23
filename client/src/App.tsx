d9decc7ebf7b44952a6fcbb533828498a7db6b26
AlinaShahmur <ashaturnaya@gmail.com>
2022-05-23 18:49:12 +0300

    Adding authentication logic, changing the components' structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import './App.css';
+import ProcessesPage from './components/Processes/ProcessesPage';
 
 function App() {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-  useEffect(() => {
-    if (!isAuthenticated) loginWithRedirect()
-  })
   return (
-    <div className="App">
-        <h1>Job application tracker</h1>
-        <Link to = '/applications'>My Applications</Link>
+    <div className="app">
+      <ProcessesPage/>
     </div>
   );
 }
