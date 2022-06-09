0215b3dc063a1efb8933238130d16ecf29f6ea47
AlinaShahmur <ashaturnaya@gmail.com>
2022-06-09 16:01:33 +0300

    Wrapping components with OutsideAlerter component and adding logic accordingly

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,26 +1,33 @@
-// import { useSelector } from 'react-redux';
 import classes from './Header.module.css'
-
 import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect, useState } from "react";
 import ProfileDropdown from './ProfileDropdown';
+import { useLocation } from 'react-router-dom';
 
 
 function Header() {
-    const {logout} = useAuth0();
     const {isAuthenticated, user} = useAuth0()
+    const location = useLocation();
     const [isProfileDropdownShown, setIsProfileDropdownShown] = useState(false);
+    useEffect(() => {
+        if (isProfileDropdownShown) setIsProfileDropdownShown(false)
+    },[location])
     return (
         <nav className = {classes.navbar}>
-            <div className={classes.logo}>My Application Tracker</div>
-            <div className = {classes["btn-logout"]}>
+            <div className={classes.logo}>
+                My Application Tracker
+            </div>
+            <div className = {classes["user-info"]}>
                 {isAuthenticated && 
                     <img onClick={() => setIsProfileDropdownShown((prev) => !prev)}
                          alt = {user ? user.name: ""} 
                          src= {user ? user.picture : ""} 
                          className={classes.avatar}
                     /> }
-                {isProfileDropdownShown && <ProfileDropdown/>}
+                {isProfileDropdownShown && <ProfileDropdown 
+                                                isOpened = {isProfileDropdownShown} 
+                                                setDropdownClosed = {() => setIsProfileDropdownShown(false)}
+                                            />}
             </div>
 
         </nav>
--- client/src/components/Header/ProfileDropdown.tsx
+++ client/src/components/Header/ProfileDropdown.tsx
@@ -1,21 +1,31 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import OutsideAlerter from '../UI/OutsideAlerter';
 import classes from './ProfileDropdown.module.css'
 
-function ProfileDropdown() {
+function ProfileDropdown(props: any) {
     const {logout} = useAuth0();
     const {isAuthenticated, user} = useAuth0()
+    const clickOutsideDropdown: any = () => {
+        if (props.isOpened) {
+            props.setDropdownClosed()
+        }
+    }
     return (
-        <div className={classes['profile-dropdown']}>
-            <ul>
-                <li>
-                    <Link to = '/profile' state = {user}>Your profile</Link>
-                </li>
-                <li className= {classes.logout}>
-                    {isAuthenticated && <button onClick={() => logout()}>Logout</button>}
-                </li>
-            </ul>
-        </div>
+        <OutsideAlerter onClickOutside = {clickOutsideDropdown}>
+            <div className={classes['profile-dropdown']}>
+                <ul>
+                    <li>
+                        <Link to = '/profile'>Your profile</Link>
+                    </li>
+                    <li className= {classes.logout}>
+                        {isAuthenticated && <button onClick={() => logout()}>Logout</button>}
+                    </li>
+                </ul>
+            </div>
+        </OutsideAlerter>
+
     )
 }
 
