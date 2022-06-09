534b3bc1b099c9deb4cb5dbfd33be08921c2fa4f
AlinaShahmur <ashaturnaya@gmail.com>
2022-06-09 16:03:10 +0300

    Wrapping Process component with ItemWrapper comp and changing styling

--- client/src/components/Processes/Process.tsx
+++ client/src/components/Processes/Process.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import classes from './Process.module.css'
 import { ICONS } from "../../utils/constants";
 import SvgIcon from "../Icons/SvgIcon";
-import classes from './Process.module.css'
+import ItemWrapper from "../UI/ItemWrapper";
 
 export default function Process(props: any) {
     const process = props.process;
     return (
-        <li className={classes.process}> 
-            <Link to = {`/${props.process._id}/applications`} state = {{process}}>{props.process.name}</Link>
-            <button onClick={() => props.onClickEditHandler(process)} style={{backgroundColor: 'white', border: 'none'}}>
-                <SvgIcon style = {{fill: "#8a996c", width: 15, height: 15}}path = {ICONS.path.edit_icon} viewBox = {ICONS.viewBox.edit_icon}/>
-            </button>
-        </li>
+        <ItemWrapper>
+            <div className={classes.process}>
+                <Link to = {`/${props.process._id}`} state = {{process}}>{props.process.name}</Link>
+                <button onClick={() => props.onClickEditHandler(process)} style={{backgroundColor: 'white', border: 'none'}}>
+                    <SvgIcon style = {{fill: "#8a996c", width: 15, height: 15}}path = {ICONS.path.edit_icon} viewBox = {ICONS.viewBox.edit_icon}/>
+                </button>
+            </div>
+        </ItemWrapper>
     )
 }
--- client/src/components/Processes/Processes.tsx
+++ client/src/components/Processes/Processes.tsx
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
 import Process from "./Process";
-import classes from './Processes.module.css'
 
 export default function Processes(props: any) {
     const storeData: any = useSelector(state => state);
     return (
-        <ul className={classes["process-list"]}>
+        <ul>
             {storeData.processes.map((process: any) => (
                 <Process onClickEditHandler = {(process: any) => props.onClickOpenEditHandler(process)} process = {process}/>
             ))}
