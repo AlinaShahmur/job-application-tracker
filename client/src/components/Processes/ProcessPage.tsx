ca8e30426fe652346a35213dc33cbb90cdcc23e9
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 21:42:43 +0300

    Adding create application modal

--- client/src/components/Processes/ProcessPage.tsx
+++ client/src/components/Processes/ProcessPage.tsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-import { BASE_URL_DEV } from "../../utils/constants";
-import { fetchData } from "../../utils/request_client";
+import { processActions } from "../../store/process-store";
+import CreateApplication from "../Applications/CreateApplication";
+import styles from './ProcessPage.module.css'
 
 export default function ProcessPage(props: any) {
     const location: any = useLocation();
-    const { process } = location.state
+    const [isCreateApplicationOpen, setIsCreateApplicationOpen] = useState(false);
+    const dispatch = useDispatch();
+    const { process } = location.state;
+    useEffect(() => {
+        dispatch(processActions.setCurrentProcess(process));
+    })
 
     return (
-        <div>
-            <Link to = {`${location.pathname}/applications`} state = {{process}}>Applications</Link>
-            <Link to = {`${location.pathname}/dashboard`} state = {{process}}>Dashboard</Link>
-        </div>
+        <React.Fragment>
+            <h1 style={{textAlign: "center", color: "#8a996c"}}>Process - {process.name}</h1>
+            <div className = {styles["process-page"]}>
+                <Link to = {`${location.pathname}/applications`} state = {{process}}>Applications</Link>
+                <Link to = {`${location.pathname}/dashboard`} state = {{process}}>Dashboard</Link>
+                <button onClick={() => setIsCreateApplicationOpen(true)}>Create Application</button>
+                {isCreateApplicationOpen && <CreateApplication isEdit = {false} onClose = {() => setIsCreateApplicationOpen(false)}/>}
+            </div>
+        </React.Fragment>
     )
 }
