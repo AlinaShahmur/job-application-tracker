5c5a0539b8c6af29e1d857b3b70400551ae01e30
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 22:13:10 +0300

    Adding attachNewProcess logic

--- src/db/classes/User.ts
+++ src/db/classes/User.ts
@@ -1,12 +1,12 @@
 import {DB_COLLECTIONS} from "../../utils/constants"
-import mongodb from 'mongodb'
+import mongodb, { ObjectId } from 'mongodb'
 import mongo from './Mongo'
 
 
 class User {
     name: string;
     email: string;
-    processes: string[];
+    processes: ObjectId[];
     collection: string = DB_COLLECTIONS.USERS
     
     static async getUserByEmail(email: string) {
@@ -31,6 +31,18 @@ class User {
                             .db
                             .collection(DB_COLLECTIONS.USERS)
                             .insertOne(newUser)
+        } catch (err) {
+            console.log(err);
+            throw new Error(err)
+        }
+    }
+
+    static async attachNewProcess(email: string, process_id: string) {
+        try {
+            return await mongo
+                            .db
+                            .collection(DB_COLLECTIONS.USERS)
+                            .updateOne({email: email}, {$addToSet: {processes: process_id}})
         } catch (err) {
             console.log(err);
             throw new Error(err)
