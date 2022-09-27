9f7d94c405b3fa26f9a2da0fdd9197caa879d3dc
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 21:43:36 +0300

    Wrapping on ErrorBoundary component

--- client/src/components/Processes/ProcessesPage.tsx
+++ client/src/components/Processes/ProcessesPage.tsx
@@ -7,32 +7,44 @@ import Processes from "./Processes";
 import ProcessModal from "./ProcessModal";
 import classes from './ProcessesPage.module.css';
 import { processActions } from "../../store/process-store";
+import ErrorBoundary from "../../utils/error-boundary";
 
 export const ProcessesPage: React.FC = () => {
     const { user, getIdTokenClaims  }: any = useAuth0();
     const dispatch = useDispatch();
-
     const [isCreateProcessShow, setIsCreateProcessShow] = useState(false);
     const [isEditProcessShow, setIsEditProcessShow] = useState(false);
   
     useEffect(() => {
       const getProcesses = async function() {
-        const token = await getIdTokenClaims();
-        console.log({token})
+        const token: any = await getIdTokenClaims();
         const data = await fetchData('get',null,`${BASE_URL}/processes/${user.email}`, token.__raw);
-        dispatch(processActions.initialLoading(data.data));
+
+        dispatch(processActions.initialLoading(data));
       }
       getProcesses();
     },[]);
-    
+
+    const onClickOpenEditHandler = (process: any) => {
+      setIsEditProcessShow(true);
+      localStorage.setItem("editingProcess", process)
+    }
+
+    const onCloseEditFormProcess = () => {
+      setIsEditProcessShow(false);
+      if (localStorage.getItem("editingProcess")) localStorage.removeItem("editingProcess");
+    }
+
     return (
+      <ErrorBoundary>
         <div className={classes['process-page']}>
             <h1>Welcome, {user.name}</h1>
             <h3>My opened job search processes:</h3>
-            <Processes onClickOpenEditHandler = {() => setIsEditProcessShow(true)}/>
+            <Processes onClickOpenEditHandler = {onClickOpenEditHandler}/>
             {isCreateProcessShow && <ProcessModal isEdit = {false} onClose = {() =>  setIsCreateProcessShow(false)}/>}
-            {isEditProcessShow && <ProcessModal isEdit = {true} onClose = {() =>  setIsEditProcessShow(false)}/>}
+            {isEditProcessShow && <ProcessModal isEdit = {true} onClose = {onCloseEditFormProcess} />}
             <button className={classes['create-new-btn']} onClick={() => setIsCreateProcessShow(true)}>Create New</button>
         </div>
+        </ErrorBoundary>
     )
 }
