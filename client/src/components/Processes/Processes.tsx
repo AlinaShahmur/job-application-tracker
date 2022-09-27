e8635cc591bc3d5a2bf50b6a4fe74ffa7e465f98
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 21:41:24 +0300

    Wrapping on ErrorBoundary component

--- client/src/components/Processes/Processes.tsx
+++ client/src/components/Processes/Processes.tsx
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux";
+import ErrorBoundary from "../../utils/error-boundary";
 import Process from "./Process";
 
 export default function Processes(props: any) {
     const processes: any = useSelector((state: any) => state.process.processes);
     
     return (
+        <ErrorBoundary>
         <ul>
             {processes.map((process: any) => (
-                <Process onClickEditHandler = {(process: any) => props.onClickOpenEditHandler(process)} process = {process}/>
+                <Process 
+                    key = {process._id} 
+                    onClickEditHandler = {(process: any) => props.onClickOpenEditHandler(process)} 
+                    process = {process}
+                />
             ))}
         </ul>
+        </ErrorBoundary>
     )
 }
