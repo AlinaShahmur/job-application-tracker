30e4dcae3d781d65ca1bb018dcfed33b354a3c8c
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 22:09:08 +0300

    Adding sync handler function

--- src/utils/dbOperations.ts
+++ src/utils/dbOperations.ts
@@ -1,14 +1,15 @@
 
-// const { TWO_WEEKS, STATUS } = require('./constants');
-// import  Application  from "../db/classes/Application";
+import Application from '../db/classes/Application';
+import mongo from '../db/classes/Mongo';
 
-import { sortTypes } from "./constants"
+import { DB_COLLECTIONS, sortTypes, STATUS, THREE_WEEKS } from "./constants"
 
 export const buildMongoQuery = (queryString, fields) => {
     const regexToMatch = queryString.length > 0 ?
     RegExp(".*" + queryString + ".*", "i") :
-    RegExp(".*")  
-    let orArray = fields.map(field => {
+    RegExp(".*") ;
+    let orArray;
+    orArray = fields.map(field => {
         return {
             [field]: regexToMatch,
         }
@@ -20,27 +21,24 @@ export const buildSortQuery = (sortType) => {
     return sortTypes[sortType]
 }
 
-// export async function updateStatus() {
-//     try {
-//         let now = new Date().getTime();
-//         let twoWeeksAgo = new Date(now-TWO_WEEKS)
-//         const pendingApproval = await Application
-//                                         .find({
-//                                             status: 'pending',
-//                                             "history.date.getTime()": { $lt: twoWeeksAgo}
-//                                         })
-        
-//         console.log(pendingApproval)            
-//         // for (let item of pendingApproval) {
-//         //     let currentTime = new Date().getTime();
-//         //     let lastStatusUpdated = item.history.date.getTime()
-//         //     if (currentTime - lastStatusUpdated > TWO_WEEKS) {
-//         //         item.status = STATUS.REJECTED;
-//         //         await Application.update(item._id, item)
-//         //     }
-//         // }
-//     } catch(err) {
-//         console.log('Error in update applications status', err);
-//     }
-
-// }
+export async function updateStatus() {
+    try {
+        const pendingApproval = await mongo
+                                        .db
+                                        .collection(DB_COLLECTIONS.APPLICATIONS)
+                                        .find({ status: 'pending'})
+                                        .toArray()
+                  
+        for (let item of pendingApproval) {
+            let currentTime = new Date().getTime();
+            const sortedHistory = item.history.sort((a,b) => a.date.getTime() > b.date.getTime());
+            let lastStatusUpdated = sortedHistory[0].date.getTime();
+            if (currentTime - lastStatusUpdated > THREE_WEEKS) {
+                item.status = STATUS.REJECTED;
+                await Application.update(item._id, item);
+            }
+        }
+    } catch(err) {
+        console.log('Error in update applications status', err);
+    }
+}
