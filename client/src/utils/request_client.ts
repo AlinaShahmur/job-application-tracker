63a132a100f703407bdd915542b4c9c79c4ad127
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 21:56:48 +0300

    Adding http response handling in fetchData function

--- client/src/utils/request_client.ts
+++ client/src/utils/request_client.ts
@@ -1,13 +1,45 @@
-import axios from "axios"
 
-const fetchData = async (method: any, body: any, url: string) => {
-    const options = {
-        url,
-        method,
-        data: body
+
+const fetchData = async (method: any, body: any, url: string, token?: any, process_id?: string) => {
+    try {
+        let options : any = {
+            method,
+            headers: {
+                "Authorization":token,
+                "Content-type":"application/json"
+            }
+        }
+        if (process_id) {
+            options.headers['process_id'] = process_id; 
+        }
+
+        if (method !== 'get') {
+            options['body'] = body;
+        } 
+
+        const res = await fetch(url,options);
+
+        if (!res.ok) throw new Error(res.statusText);
+
+        const response: any = await res.json();
+
+        const responseStatus: number = response.hasOwnProperty("status") 
+                                        ? response.status
+                                        : res.status;
+
+        const isResponseOk = response.hasOwnProperty("success") 
+                            ? response.success
+                            : true
+        
+        if (!isResponseOk || responseStatus >= 300) throw new Error("Error when fetching");
+        if (response.data) return response.data;
+        
+        return response
+    } catch(e) {
+        console.log("Error in fetch", e);
+        throw e
     }
-    const res = await axios(options)
-    return res.data
+
 }
 
 export {fetchData}
