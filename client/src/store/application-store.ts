90aca659a932326e7f66f4f7e825004013c0c983
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 21:55:08 +0300

    Adding totalItems parameter to application store in Redux

--- client/src/store/application-store.ts
+++ client/src/store/application-store.ts
@@ -1,16 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const applicationsInitialState = {applications: []};
+const applicationsInitialState = {applications: [], totalItems:0};
 
 const applicationSlice = createSlice({
     name: 'application',
     initialState: applicationsInitialState,
     reducers: {
         initialLoading(state, action) {
-            state.applications = action.payload // it's allowed because in redux-toolkit we can't affect to state directly (it makes copy of the current state)
+            state.applications = action.payload; // it's allowed because in redux-toolkit we can't affect to state directly (it makes copy of the current state)
+        },
+        setTotalItems(state, action) {
+            state.totalItems = action.payload;
         }
     }
 });
 
-export const applicationActions = applicationSlice.actions
-export default applicationSlice.reducer
+export const applicationActions = applicationSlice.actions;
+export default applicationSlice.reducer;
