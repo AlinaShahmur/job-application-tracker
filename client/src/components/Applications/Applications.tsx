bcc4a2ce97c8e8b05472d6318dda86de09f00e65
AlinaShahmur <ashaturnaya@gmail.com>
2022-09-27 22:17:21 +0300

    Replacing retrieving process from location state on redux

--- client/src/components/Applications/Applications.tsx
+++ client/src/components/Applications/Applications.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation, useParams } from 'react-router-dom';
 import { BASE_URL } from '../../utils/constants';
 import { fetchData } from '../../utils/request_client';
 import Loader from '../UI/Loader';
@@ -11,9 +10,10 @@ import TableHeader from '../UI/TableHeader';
 import ApplicationItem from './ApplicationItem';
 import styles from './Applications.module.css'
 
-function Applications(props: any) {
+function Applications() {
 
     const { getIdTokenClaims } = useAuth0();
+    
     const [applications, setApplications] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [limit, setLimit] = useState(10);
@@ -22,19 +22,16 @@ function Applications(props: any) {
     const [isLoading, setIsLoading] = useState(true);
     const [searchQuery, setSearchQuery] = useState('');
     const [isUpSort, setIsUpSort] = useState(false);
-    const [currentProcess, setCurrentProcess] = useState({});
-    const location: any = useLocation()
-    const { process } = location.state
+    const process = useSelector((state: any) => state.process.currentProcess);
+
     useEffect( () => {
         async function getApplications() {
             const token: any = await getIdTokenClaims();
-            console.log({token})
-            const query = [{key:'skip', value:0},{key:'limit', value:10},{key:'queryString', value:''}]
             const url = `${BASE_URL}/applications?skip=${(currentPage-1)*limit}&limit=${limit}&queryString=${searchQuery}&sortType=${isUpSort ? 'desc' : 'asc'}`
-            const res = await fetchData('get', null, url, token.__raw)
-            console.log("res", res)
-            setPages(Math.ceil(res.totalCount / limit))
-            setApplications(res.data);
+            const res = await fetchData('get', null, url, token.__raw, process._id);
+            
+            setPages(Math.ceil(res.totalItems / limit))
+            setApplications(res.applications);
             setIsLoading(false);
         }
         getApplications();
@@ -42,7 +39,6 @@ function Applications(props: any) {
     },[currentPage,searchQuery,isUpSort])
 
     const onSearchHandler = (searchString: string) => {
-        console.log(searchString)
         setCurrentPage(1);
         setSearchQuery(searchString);
     }
@@ -53,7 +49,7 @@ function Applications(props: any) {
         setIsUpSort(isUpSort);
     }
     const appContent = applications.length > 0 ? 
-                        applications.map((app: any) => <ApplicationItem item = {app}/>) :
+                        applications.map((app: any) => <ApplicationItem key = {app._id} item = {app}/>) :
                         <h2>There are no applications</h2>
     return (
         <div className = {styles.applications}>
