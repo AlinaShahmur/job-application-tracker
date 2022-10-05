941589b84b2edc1c18c505de1ec78544723c6d45
AlinaShahmur <ashaturnaya@gmail.com>
2022-10-05 16:45:45 +0300

    Adding deleteProcess action to process slice in redux store

--- client/src/store/process-store.ts
+++ client/src/store/process-store.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const processesInitialState = {processes: [], currentProcess: null};
+const processesInitialState = {processes: [], currentProcess: null, isFetched: false};
 
 const processSlice = createSlice({
     name: 'process',
@@ -8,6 +8,15 @@ const processSlice = createSlice({
     reducers: {
         initialLoading(state, action) {
             state.processes = action.payload // it's allowed because in redux-toolkit we can't affect to state directly (it makes copy of the current state)
+            state.isFetched = true
+        },
+        deleteProcess(state, action) {
+            const copyOfProcesses = [...state.processes];
+            const index = copyOfProcesses.findIndex((item: any) => item._id === action.payload);
+            if (index > 0) {
+                copyOfProcesses.splice(index, 1);
+                state.processes = copyOfProcesses;
+            }
         },
         setCurrentProcess(state, action) {
             state.currentProcess = action.payload;
