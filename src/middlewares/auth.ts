8c4f24801ed003385ffaa137747770b6ad6bfe06
AlinaShahmur <ashaturnaya@gmail.com>
2022-10-28 22:00:28 +0300

    Changing the source of public key in middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,11 +3,15 @@ const jwt = require('jsonwebtoken');
 import path from 'path';
 
 const certPath = path.join( __dirname, '../../certs/public.key');
+const publicKey = process.env.PUBLIC_KEY;
+console.log({publicKey});
+const cert = readFileSync(certPath, 'utf-8');
+console.log({cert});
+
 
 export function auth(req, res, next) {
-    const cert = readFileSync(certPath, 'utf-8');
     const token = req.headers['authorization'];
-
+    const cert = process.env.PUBLIC_KEY;
     jwt.verify(token, cert, function(err, decoded) {
         console.log({err})
         if (!decoded) return res.status(401).send("The token is not valid");
