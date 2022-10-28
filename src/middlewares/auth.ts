1e5ce4069c0889bae7b759dad0886dfcc6008898
AlinaShahmur <ashaturnaya@gmail.com>
2022-10-28 22:13:40 +0300

    Adding logs

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 export function auth(req, res, next) {
     const token = req.headers['authorization'];
     const cert = process.env.PUBLIC_KEY;
+    console.log({cert});
+    
     jwt.verify(token, cert, function(err, decoded) {
         console.log({err})
         if (!decoded) return res.status(401).send("The token is not valid");
