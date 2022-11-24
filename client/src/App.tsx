3047d91fc1d23a9bbff7f2fffbae4c5afc6243e8
AlinaShahmur <ashaturnaya@gmail.com>
2022-11-24 15:41:52 +0200

    Adding logic to register new user in database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
 import './App.css';
 import {ProcessesPage} from './components/Processes/ProcessesPage';
+import { BASE_URL } from './utils/constants';
 import ErrorBoundary from './utils/error-boundary';
+import { fetchData } from './utils/request_client';
 
 function App() {
+  const { user, getIdTokenClaims  }: any = useAuth0();
+  useEffect(() => {
+    const createUserIfNeed = async function(email: string) {
+      const token: any = await getIdTokenClaims();
+      await fetchData('GET',null, `${BASE_URL}/api/users/${email}`, token.__raw);
+    }
+    createUserIfNeed(user.email);
+  })
   return (
     <ErrorBoundary>
       <div className="app">
         <ProcessesPage/>
       </div>
-      </ErrorBoundary>
+    </ErrorBoundary>
   );
 }
 
