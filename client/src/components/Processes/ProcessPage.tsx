51a9b13682f1fb8ca20f9d14c1f85dbbb89f6266
AlinaShahmur <ashaturnaya@gmail.com>
2022-11-30 22:32:20 +0200

    Changing the layout of Process Page

--- client/src/components/Processes/ProcessPage.tsx
+++ client/src/components/Processes/ProcessPage.tsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { processActions } from "../../store/process-store";
-import { BASE_URL } from "../../utils/constants";
+import { BASE_URL, ICONS } from "../../utils/constants";
 import { fetchData } from "../../utils/request_client";
 import { buildDeleteConfirmationObject } from "../../utils/utils";
 import CreateApplication from "../Applications/CreateApplication";
+import SvgIcon from "../Icons/SvgIcon";
 import Alert from "../UI/Alert";
+import ItemWrapper from "../UI/ItemWrapper";
 import styles from './ProcessPage.module.css'
 
 export default function ProcessPage(props: any) {
@@ -36,16 +38,54 @@ export default function ProcessPage(props: any) {
         <React.Fragment>
             <h1 style={{textAlign: "center", color: "#8a996c"}}>Process - {process.name}</h1>
             <div className = {styles["process-page"]}>
-                <Link to = {`${location.pathname}/applications`} state = {{process}}>Applications</Link>
-                <Link to = {`${location.pathname}/dashboard`} state = {{process}}>Dashboard</Link>
-                <button onClick={() => setIsCreateApplicationOpen(true)}>Create Application</button>
-                {isCreateApplicationOpen && <CreateApplication isEdit = {false} onClose = {() => setIsCreateApplicationOpen(false)}/>}
-                {isDeleteConfirmationOpen && <Alert 
+                <ul>
+                    <ItemWrapper>
+                            <Link to = {`${location.pathname}/applications`} state = {{process}}>
+                                Applications   
+                                <SvgIcon path = {ICONS.path.file_icon}
+                                    style = {{width: 13, height: 13, fill: "#000000", marginLeft: 10}}
+                                    viewBox = {ICONS.viewBox.file_icon}
+                                />                       
+                            </Link>
+
+                    </ItemWrapper>
+
+                    <ItemWrapper>
+
+                        <Link to = {`${location.pathname}/dashboard`} state = {{process}}>
+                            Dashboard                           
+                            <SvgIcon path = {ICONS.path.dashboard_icon}
+                                style = {{width: 13, height: 13, fill: "#000000", marginLeft: 10}}
+                                viewBox = {ICONS.viewBox.dashboard_icon}
+                            />  
+                        </Link>
+                    </ItemWrapper>
+                    <ItemWrapper>
+                        <button className={styles["process-action-btn"]} onClick={() => setIsCreateApplicationOpen(true)}>
+                            Create Application
+                            <SvgIcon path = {ICONS.path.plus_icon}
+                                style = {{width: 13, height: 13, fill: "#000000", marginLeft: 10}}
+                                viewBox = {ICONS.viewBox.plus_icon}
+                            />  
+                        </button>
+                    </ItemWrapper>
+                    <ItemWrapper>
+                        <button className={styles["process-action-btn"]} onClick={() => setIsDeleteConfirmationOpen(true)}>
+                            Delete Process
+                            <SvgIcon path = {ICONS.path.trash_can_icon}
+                                style = {{width: 13, height: 13, fill: "#000000", marginLeft: 10}}
+                                viewBox = {ICONS.viewBox.trash_can_icon}
+                            />  
+                        </button>
+                    </ItemWrapper>
+                    
+                </ul>
+            {isCreateApplicationOpen && <CreateApplication isEdit = {false} onClose = {() => setIsCreateApplicationOpen(false)}/>}
+            {isDeleteConfirmationOpen && <Alert 
                                                 {...buildDeleteConfirmationObject('process')} 
-                                                onBtnClick = {deleteProcessHandler} 
-                                                onClose = {() => setIsDeleteConfirmationOpen(false)}
-                                            />}
-                <button onClick={() => setIsDeleteConfirmationOpen(true)} className="danger">Delete Process</button>
+                                                    onBtnClick = {deleteProcessHandler} 
+                                                    onClose = {() => setIsDeleteConfirmationOpen(false)}
+                                        />}
             </div>
         </React.Fragment>
     )
