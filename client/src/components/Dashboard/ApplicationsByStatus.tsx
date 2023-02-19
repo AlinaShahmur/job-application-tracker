dfdfe0aa40934ccc1170c63dfa34ae1a89cce4c2
AlinaShahmur <ashaturnaya@gmail.com>
2023-02-19 17:07:51 +0200

    Added Legend to chart diagram

--- client/src/components/Dashboard/ApplicationsByStatus.tsx
+++ client/src/components/Dashboard/ApplicationsByStatus.tsx
@@ -2,10 +2,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { VictoryContainer, VictoryPie } from "victory";
+import { VictoryContainer, VictoryLegend, VictoryPie } from "victory";
 import { BASE_URL, STATUS_AND_COLORS } from "../../utils/constants";
 import { fetchData } from "../../utils/request_client";
 import styles from './ApplicationsByStatus.module.css';
+
 
 
 
@@ -19,7 +20,6 @@ function ApplicationsByStatus() {
         const fetchApplicationsQtyByStatuses = async () => {
             const token: any = await getIdTokenClaims();                
             const res = await fetchData('GET', null,`${BASE_URL}/api/applications/byStatuses`, token.__raw, process._id);
-            console.log({res});
             
             setPending(res.pending);
             setRejected(res.rejected)
@@ -27,22 +27,43 @@ function ApplicationsByStatus() {
         fetchApplicationsQtyByStatuses()
     }, []);
 
+    const chartData = [{title: "Pending", value: pending, color: STATUS_AND_COLORS.pending}, {title: "Rejected", value: rejected, color: STATUS_AND_COLORS.rejected}];
+
     return (
         <div className={styles["applications-by-status"]}>
             <h3>Applications in the "Status" context</h3>
+            <svg width={270} height={400}>
+                <VictoryLegend x={70} y={10}
+                        centerTitle
+                        borderPadding={0}
+                        standalone = {false}
+                        orientation="horizontal"
+                        gutter={20}
+                        
+                        data = {chartData.filter(x => x.value > 0).map(x => {
+                            return {name: x.title, symbol: {fill: x.color}}
+                        })}
+                    />
+                    <VictoryPie 
+                        colorScale={[STATUS_AND_COLORS.pending, STATUS_AND_COLORS.rejected ]}  
+                        data={chartData.map(x => {
+                            return {x: x.title, y: x.value}
+                        })}
 
-            <VictoryPie 
-                colorScale={[STATUS_AND_COLORS.pending, STATUS_AND_COLORS.rejected ]}  
-                data={[
-                    { x: "Pending", y: pending },
-                    { x: "Rejected", y: rejected }
-                ]}
-                width={400} height={300}
-                style={{ labels: { fontSize: 12 } }}
-                labelRadius={10}
-                labels = {({datum}) => datum.y > 0 ? `${datum.x} - ${datum.y}` : ""}
-                containerComponent={<VictoryContainer responsive={false}/>}
-            />
+                        width={270} height={300}
+                        style={{ labels: { fontSize: 18 } }}
+                        padding = {{
+                            top: 100,
+                            left: 60
+                        }}
+                        labelRadius={10}
+                        standalone = {false}
+                        labels = {({datum}) => datum.y > 0 ? `${datum.y}` : ""}
+                        containerComponent={<VictoryContainer responsive={false}/>}
+                    />
+            </svg>
+                   
+
         </div>
     )
 }
